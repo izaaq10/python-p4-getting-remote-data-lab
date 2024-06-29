21748f68cd4458a846b4dcb15b1559a83a861395
Izaaq <odongoizaaq@gmail.com>
2024-06-29 05:12:56 -0400

    commit

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -2,12 +2,24 @@ import requests
 import json
 
 class GetRequester:
-
     def __init__(self, url):
         self.url = url
 
     def get_response_body(self):
-        pass
+        try:
+            with requests.get(self.url) as response:
+                response.raise_for_status()
+                return response.text
+        except requests.exceptions.RequestException as e:
+            print(f"Error fetching data: {e}")
+            return None
 
     def load_json(self):
-        pass
+        response_body = self.get_response_body()
+        if response_body:
+            try:                return json.loads(response_body)
+            except json.JSONDecodeError as e:
+                print(f"Error decoding JSON: {e}")
+                return None
+        else:
+            return None
